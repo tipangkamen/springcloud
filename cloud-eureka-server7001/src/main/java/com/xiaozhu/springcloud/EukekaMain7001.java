70612505f3aecda6dc1bd0e804bac80ee88898fa
zhuguancheng <1005071600@qq.com>
2021-06-25 01:07:21 +0800

    Eureka服务端配置完成，没有配置devtools，明天需要配完

--- cloud-eureka-server7001/src/main/java/com/xiaozhu/springcloud/EukekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/xiaozhu/springcloud/EukekaMain7001.java
@@ -2,10 +2,13 @@ package com.xiaozhu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class PaymentMain8001 {
+//标识是Eureka服务端
+@EnableEurekaServer
+public class EukekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EukekaMain7001.class,args);
     }
 }
